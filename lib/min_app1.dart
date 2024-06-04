90f796be59e5ac541519a2e2b9a849c279c6c67a
Dicksen Veloopillay <d.veloopillay@telecom.mu>
2024-06-04 15:19:42 +0400

    change view name

--- lib/min_app1.dart
+++ lib/min_app1.dart
@@ -2,15 +2,15 @@ library min_app1;
 
 import 'package:flutter/material.dart';
 
-// class MiniApp1View extends StatelessWidget {
-//   const MiniApp1View({super.key});
+class MiniApp1View extends StatelessWidget {
+  const MiniApp1View({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Mini App 1'),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mini App 1'),
+      ),
+    );
+  }
+}
