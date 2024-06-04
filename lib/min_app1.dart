1324dc2cd77cae3e83e7531d9c0865aac78a7ea8
Dicksen Veloopillay <d.veloopillay@telecom.mu>
2024-06-04 15:05:29 +0400

    change view name

--- lib/min_app1.dart
+++ lib/min_app1.dart
@@ -9,9 +9,8 @@ class MiniApp1View extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Mini app 1'),
+        title: const Text('Mini App 1'),
       ),
-      body: Text('Hello from App 1'),
     );
   }
 }
