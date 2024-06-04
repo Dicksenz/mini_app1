8f4766f42144556928cf9093b97e2281aab26a8e
Dicksen Veloopillay <d.veloopillay@telecom.mu>
2024-06-04 14:29:49 +0400

    initial commit

---
+++ test/min_app1_test.dart
@@ -0,0 +1,12 @@
+// import 'package:flutter_test/flutter_test.dart';
+
+// import 'package:min_app1/min_app1.dart';
+
+// void main() {
+//   test('adds one to input values', () {
+//     final calculator = Calculator();
+//     expect(calculator.addOne(2), 3);
+//     expect(calculator.addOne(-7), -6);
+//     expect(calculator.addOne(0), 1);
+//   });
+// }
